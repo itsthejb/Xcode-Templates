84aa724849898d638cd9321ca5eddf45893257ea
Jonathan Crooke <joncrooke@gmail.com>
2013-11-27 13:02:40 +0000

    Initial

---
+++ Memrise/Spec.xctemplate/XCTestCase/___FILEBASENAME___.m
@@ -0,0 +1,34 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ___FILEBASENAMEASIDENTIFIER___ : ___VARIABLE_testSubclass___
+
+@end
+
+@implementation ___FILEBASENAMEASIDENTIFIER___
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here; it will be run once, before the first test case.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here; it will be run once, after the last test case.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
