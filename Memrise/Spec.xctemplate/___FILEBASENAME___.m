20a8a494a4446b263853e4c9afbaff180ba3e10a
Jonathan Crooke <joncrooke@gmail.com>
2013-11-27 13:38:53 +0000

    Spec template

---
+++ Memrise/Spec.xctemplate/___FILEBASENAME___.m
@@ -0,0 +1,21 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+#import "MemriseBaseSpec.h"
+
+SpecBegin(___FILEBASENAMEASIDENTIFIER___)
+
+describe(@"<#name#>", ^{
+
+  it(@"<#should#>", ^{
+  
+  });
+
+});
+
+SpecEnd
